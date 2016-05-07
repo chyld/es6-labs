b99e80e0c21b967ab7adb1922f1a3331479542d9
Chyld Medford <chyld.medford@gmail.com>
2016-05-07 00:43:44 -0700

    spaces

--- 001-const-let.ts
+++ 001-const-let.ts
@@ -3,29 +3,28 @@ let a = 1;
 console.log('1:', a);
 
 (function(){
-    let a = 2;
+  let a = 2;
+  console.log('1:', a);
+  
+  {
+    let a = 3;
     console.log('1:', a);
-    
-    {
-        let a = 3;
-        console.log('1:', a);
-    }
+  }
 })();
 
 {
-    let a = 4;
-    console.log('1:', a);
+  let a = 4;
+  console.log('1:', a);
 }
 
 let fns = [];
 
-{
-    
-    for(let a = 5; a < 10; a++){
-        fns.push(() => a);
-    }
+{    
+  for(let a = 5; a < 10; a++){
+    fns.push(() => a);
+  }
 }
 
 for(let f of fns){
-    console.log('f:', f.call());
+  console.log('f:', f.call());
 }
